de4a73a2d5a27d58318a0da0d5023c88738f867f
Tilaktiwari <tiwari5tilak@gmail.com>
2020-10-14 01:22:03 -0400

    This is our team3

---
+++ src/HelloTeam.java
@@ -0,0 +1,7 @@
+public class HelloTeam {
+    public static void main(String[] args) {
+        System.out.println("Hello all  from team 3 please join in our group");
+        System.out.println("Good luck for everyone for mid term");
+    }
+}
+
